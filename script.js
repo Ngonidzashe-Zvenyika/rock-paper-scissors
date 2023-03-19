7eef4790a35cb24d052526d10f7eab0618c61128
Ngonidzashe <123906490+Ngonidzashe-Zvenyika@users.noreply.github.com>
2023-03-19 22:24:08 +0200

    Add player's input and validation in script.js

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 console.log("Rock-Paper-Scissors")
-let computerChoice = undefined;
+
+// This loop validates the player's decision.
+
+let playerChoice = prompt("Rock, Paper or Scissors?").toLowerCase();
+while ((playerChoice !== "rock") && (playerChoice !== "paper") && (playerChoice !== "scissors")) {
+    alert("Please check your spelling and try again.")
+    playerChoice = prompt("Rock, Paper or Scissors?").toLowerCase();
+}
 
 // This function allows the computer to randomly choose rock, paper or scissors.
 
+let computerChoice = undefined;
 function getComputerChoice() {
     randomNumber = Math.ceil(Math.random() * 3)
     if (randomNumber === 1) {
@@ -13,6 +21,4 @@ function getComputerChoice() {
         return computerChoice = "scissors";
     }
 }
-
-
 
