60a2de8af7216817e8f9839c84aff2d67f1a16f9
Ngonidzashe <123906490+Ngonidzashe-Zvenyika@users.noreply.github.com>
2023-03-19 21:22:52 +0200

    Add getComputerChoice function to script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log("Rock-Paper-Scissors")
+let computerChoice = undefined;
+
+// This function allows the computer to randomly choose rock, paper or scissors.
+
+function getComputerChoice() {
+    randomNumber = Math.ceil(Math.random() * 3)
+    if (randomNumber === 1) {
+        return computerChoice = "rock";
+    } else if (randomNumber === 2) {
+        return computerChoice = "paper";
+    } else {
+        return computerChoice = "scissors";
+    }
+}
+
+
+
